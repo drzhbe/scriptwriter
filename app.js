4be07758eeff55767d40641a8c16f89b0a96f5f3
Stas <stanislav@flurosat.com>
2022-02-21 13:24:04 +1100

    add paste listener

--- app.js
+++ app.js
@@ -1,37 +1,57 @@
+console.log(`
+Todo:
+1. When the current page doesn't have any more space, create a new one.
+2. Export to PDF. Consider browser's print option.
+https://www.smashingmagazine.com/2015/01/designing-for-print-with-css/
+https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=Hello%20world&choe=UTF-8
+3. Collaborative editing.
+`);
+
+const init = () => {
+  // Prevent default browser's tab action.
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Tab") {
+      e.preventDefault();
+    }
+  });
+  document.addEventListener("keyup", (e) => {
+    if (e.key === "Tab") {
+      e.preventDefault();
+      setElementNextMode(getActiveElement());
+    }
+  });
+  document.addEventListener("paste", (e) => {
+    const text = e.clipboardData.getData("text");
+    console.log("### text pasted", text);
+  });
+
+  const mutationObserver = new MutationObserver((mutations) => {
+    mutations.forEach((m) => {
+      console.log("@ m", m);
+      m.addedNodes.forEach((node) => {
+        if (node.nodeName === "DIV") {
+          setElementNextMode(node, true);
+        }
+      });
+    });
+  });
+  const documentElement = document.querySelector(".document");
+  mutationObserver.observe(documentElement, {
+    childList: true,
+    subtree: true,
+  });
+};
+
+init();
+
+// ---
+
 const getActiveElement = () => {
   const activeElement = document.getSelection().getRangeAt(0).endContainer;
   return activeElement.nodeName === "#text"
     ? activeElement.parentElement
     : activeElement;
 };
-
-// Prevent default browser's tab action.
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Tab") {
-    e.preventDefault();
-  }
-});
-document.addEventListener("keyup", (e) => {
-  if (e.key === "Tab") {
-    e.preventDefault();
-    setElementNextMode(getActiveElement());
-  }
-});
-
-const mutationObserver = new MutationObserver((mutations) => {
-  mutations.forEach((m) => {
-    m.addedNodes.forEach((node) => {
-      if (node.nodeName === "DIV") {
-        setElementNextMode(node, true);
-      }
-    });
-  });
-});
-const documentElement = document.querySelector(".document");
-mutationObserver.observe(documentElement, {
-  childList: true,
-  subtree: true,
-});
 
 const MODES = {
   Action: 0,
